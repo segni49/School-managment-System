2a1b2382d425f3669d93c5198e86f128d8e0ffef
segni_abera <segniab69@gmail.com>
2025-05-10 09:45:56 +0300

    Remove unused authentication handlers and add NextAuth options configuration

--- auth.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import db from "@/lib/database/db";
-import authConfig from "./auth.config";
-
-export const {
-    handlers: {GET, POST},
-    auth, SignIn, SignOut;
-}
